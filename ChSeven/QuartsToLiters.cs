8221e5228765aa18afd8a8d8bd445239e9a598fc
Tyler Rench <tyrench94@gmail.com>
2015-12-11 10:14:19 -0600

    ChSeven_Quarts
    ChSeven_Quarts

---
+++ ChSeven/QuartsToLiters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertQuartsToLiters
+{
+    class QuartsToLiters
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double quarts;
+
+            WriteLine("How many quarts do you have?  ");
+            input = ReadLine();
+            quarts = Convert.ToDouble(input);
+
+            WriteLine(DisplayQuartsToLiters(quarts));
+        }
+        private static double DisplayQuartsToLiters(double quartMethod)
+        {
+            double totalLiters;
+            const double quartToLiters = 0.966353;
+
+            totalLiters = quartMethod * quartToLiters;
+            return totalLiters;
+            
+        }
+    }
+}
