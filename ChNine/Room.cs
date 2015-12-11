91f68fa1d734cf481da376900f0adb599b185b3d
Tyler Rench <tyrench94@gmail.com>
2015-12-11 10:19:42 -0600

    ChNine_Squarefeet
    ChNine_Squarefeet

---
+++ ChNine/Room.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilingDemo
+{
+    class Room 
+    {
+        int width { get; }
+        int length { get; }
+        int floorArea;
+        int boxes;
+
+
+        public Room(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int getFloorArea()
+        {
+            floorArea = length * width;
+            return floorArea;
+        }
+
+        private int cBoxes(int squareFeet)
+        {
+            int quad = squareFeet / 12;
+            int remainder = squareFeet % 12;
+            int extra;
+            if (remainder == 0)
+            {
+                extra = 0;
+            }
+            else
+            {
+                extra = 1;
+            }
+            return quad + extra + 1;
+        }
+        public int getBoxes()
+        {
+            return cBoxes(floorArea);
+        }
+
+    }
+}
---
+++ ChNine/TilingDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilingDemo
+{
+    class TilingDemo
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("What is the length?  ");
+            int x = Convert.ToInt32(ReadLine());
+            WriteLine("What is the height of the room?  ");
+            int y = Convert.ToInt32(ReadLine());
+            Room squareFeet = new Room(x,y);
+
+
+            Write("The square feet of your room is {0} and",squareFeet.getFloorArea());
+            Write(" you would need {0} many boxes.", squareFeet.getBoxes());
+
+
+
+
+
+        }
+    }
+}
