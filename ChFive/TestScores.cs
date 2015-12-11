ece666036cda00ef158b180f3259c72a8190f078
Tyler Rench <tyrench94@gmail.com>
2015-12-11 09:28:43 -0600

    ChFive_Looping
    ChFive_Looping

---
+++ ChFive/TestScores.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestScores
+{
+    class TestScores
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            int total = 0;
+            int scoreCounter = 0;
+
+            Console.WriteLine("Welcome to the test score average calculator!");
+
+            while (score != 101)
+            {
+                Console.WriteLine("Enter a score or enter 101 to get the total and average: ");
+                int.TryParse(Console.ReadLine(), out score);
+
+                if ((score < 0) || (score > 100))
+                {
+                    Console.WriteLine("Score Entered was not in between 0 and 100!");
+                }
+                else
+                {
+                    total = total + score;
+                    scoreCounter++;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
